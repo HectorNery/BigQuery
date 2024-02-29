4becb6ffa32b8b91accfc73aedb595738999b296
HectorNery <101744251+HectorNery@users.noreply.github.com>
2024-02-29 11:28:54 -0600

    Add files via upload

---
+++ app_github.py
@@ -0,0 +1,40 @@
+#Para familiarizarse más con BigQuery, ahora emitirás una consulta 
+#en el conjunto de datos públicos de GitHub . 
+#Encontrarás los mensajes de confirmación más comunes en GitHub. 
+#También usarás la consola web de BigQuery para obtener una vista previa 
+#y ejecutar consultas ad-hoc.
+
+#Para ver como se ven os datos abre el conjunto de datos Github
+#En la la vista gráfica de BigQueary
+#https://console.cloud.google.com/bigquery?p=bigquery-public-data&d=github_repos&t=commits&page=table
+
+#Para ver cómo se ven los datos, abra el conjunto de datos 
+#de GitHub en la IU web de BigQuery:
+    
+from google.cloud import bigquery
+
+client = bigquery.Client()
+
+query = """
+    SELECT
+  year AS Anio,
+  major_category AS CategoriaPrincipal,
+  borough AS Distrito,
+  SUM(value) AS TotalDelitos
+FROM
+  `bigquery-public-data.london_crime.crime_by_lsoa`
+WHERE
+  year IS NOT NULL
+  AND major_category IS NOT NULL
+  AND borough IS NOT NULL
+GROUP BY
+  Anio, CategoriaPrincipal, Distrito
+ORDER BY
+  Anio, TotalDelitos DESC;
+"""
+results = client.query(query)
+
+for row in results:
+    subject = row['subject']
+    num_duplicates = row['num_duplicates']
+    print(f'{subject:<20} | {num_duplicates:>9,}')
